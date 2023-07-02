d77d18332774c56f2d4a6af8fde6894ba308e636
可汗 <njyukehan@gmail.com>
2023-07-02 22:06:57 +0800

    Add files via upload

---
+++ rename.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+if [ -z "$1" ]
+then
+  echo "Please provide the directory as an argument."
+  exit 1
+fi
+
+# 指定源文件夹路径
+source_folder="$1"
+
+# 指定目标文件夹路径
+target_folder="~/Downloads/merged"
+
+# 创建目标文件夹
+mkdir -p ${target_folder}
+
+for file in "$source_folder"/*.mkv
+do
+  echo "Processing $file"
+  filename=$(basename -- "$file")
+  filename="${filename%.*}"
+  ffmpeg -i "$file" -c:v libx264 -crf 23 -c:a aac "$1/$filename.mp4"
+done
