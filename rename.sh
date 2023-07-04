32b9066835f9b3df089b4c5446d89c8407466db2
可汗 <njyukehan@gmail.com>
2023-07-04 22:22:23 +0800

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 if [ -z "$1" ]
 then
@@ -9,27 +9,35 @@ fi
 # 指定源文件夹路径
 source_folder="$1"
 
+# 获取当前路径
+current_dir=$(pwd)
+
 # 指定目标文件夹路径
-target_folder="~/Downloads/merged"
+target_folder="${current_dir}/merged"
 
 # 创建目标文件夹
 mkdir -p ${target_folder}
 
-# 使用 find 命令查找指定源文件夹内的所有 MKV 文件，并进行循环处理
-find ${source_folder} -type f -name "*.mkv" | while read -r mkv_file; do
-  # 提取 MKV 文件名（不包含扩展名）
-  mkv_name=$(basename "$mkv_file" .mkv)
+# 设置`nullglob`选项，使得在没有匹配的文件时不会报错
+shopt -s nullglob
+for file in "$source_folder"/*.mp4 "$source_folder"/*.mkv
+do
+  filename=$(basename -- "$file")
+  echo "Processing ${filename}"
+  filename="${filename%.*}"
   
   # 使用 find 命令查找同名的 ASS 文件
   ass_file=$(find "$source_folder" -type f -name "${mkv_name}.ass")
   
-  # 如果找到了对应的 ASS 文件
-  if [ -f "$ass_file" ]; then
-    # 合并 ASS 字幕到 MKV 文件中，并将输出文件放入目标文件夹
-    output_file="${target_folder}/${mkv_name}.mkv"
-    ffmpeg -i "${source_folder}/${mkv_name}.mkv" -i "$ass_file" -c copy -metadata:s:s:0 language=chi "$output_file"
-    echo "Merged $ass_file into $output_file"
-  else
-    echo "No ASS file found for $mkv_file"
-  fi
+  如果找到了对应的 ASS 文件
+# if [ -f "$ass_file" ]; then
+#   # 合并 ASS 字幕到 MKV 文件中，并将输出文件放入目标文件夹
+#   output_file="${target_folder}/${mkv_name}.mkv"
+#   ffmpeg -i "${source_folder}/${mkv_name}.mkv" -i "$ass_file" -c copy -metadata:s:s:0 language=chi "$output_file"
+#   echo "Merged $ass_file into $output_file"
+# else
+#   echo "No ASS file found for $mkv_file"
+# fi
+  
 done
+shopt -u nullglob
